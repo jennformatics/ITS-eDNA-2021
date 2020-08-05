da105ee8ac0bf492011963af0241a719477009e5
Jennifer Drummond <jenn@JennMini.local>
2020-08-05 12:33:46 -0500

    Tidy 'final' version of usearch clustering pipeline, and add vsearch version at bottom for comparison.

--- 02-otu-generation/merge-and-cluster.sh
+++ 02-otu-generation/merge-and-cluster.sh
@@ -1,47 +1,37 @@
-. ~/init/underc
+SP=`basename $PWD`
 
-EDNA=$HOME/spe-software/notre-dame/eDNA_pipeline_v3.7
-D=/storage/hpc/work/spe1/data/edna
-ACDIR=/projects/ac53/underc
-PROJDIR=/projects/spe1/edna
-METADIR=/projects/spe1/edna/metadata
+. ~/init/underc $SP
 
-SCRATCHDIR=/scratch/jenn/underc
-MAINDIR=$SCRATCHDIR/for-realz
-RSRCDIR=$MAINDIR/rsrc
-SCRIPTSDIR=$MAINDIR/scripts
-LOGDIR=$MAINDIR/logs
-BLASTDIR=$MAINDIR/blast
-DEPRXDATA=$MAINDIR/plantITS_FR_fq
-
-mkdir -p $MAINDIR/blast
-mkdir -p $LOGDIR/deprx
-cd $MAINDIR
-ln -s /home/jenn/scripts/underc scripts
-mkdir -p rsrc; cd rsrc
-for R in $PROJDIR/rsrc/*.{dmp,fas}; do ln -s $R; done
-ln -s $METADIR/sample-names-and-order.txt
-# Keep this as a backup somewhere: tar -hczf underc-basic-rsrc.tgz rsrc
-
-# OR:
-cd $MAINDIR
-rmdir rsrc
-tar -xzf $PROJDIR/metadata/underc-basic-rsrc.tgz
+# PROJNAME=underc
+# 
+# EDNA=$SOFTWARE/notre-dame/eDNA_pipeline_v3.7
+# D=/storage/hpc/work/spe1/data/edna
+# ACDIR=/projects/ac53/underc
+# PROJDIR=/projects/spe1/edna
+# METADIR=/projects/spe1/edna/metadata
+# 
+# SCRATCHDIR=/scratch/jenn/underc
+# MAINDIR=$SCRATCHDIR/$1
+# RSRCDIR=$MAINDIR/rsrc
+# SCRIPTSDIR=$MAINDIR/scripts
+# LOGDIR=$MAINDIR/logs
+# BLASTDIR=$MAINDIR/blast
+# DEPRXDATA=$MAINDIR/plantITS_FR_fq
+# 
+# cd $MAINDIR
+# echo "MAINDIR is now $MAINDIR."
 
 
 #####################################
 ###  Read merging and clustering  ###
 #####################################
 
-# Check out difference between usearch -fastq_mergepairs and...what else do we use? DADA? Pear? BBDuk?
-
 # Merging, filtering, and fq/fa conversion (keeping the filt.fq for later reference).
 # This is the crazy, hairy way to process stderr on its way into the log:   >> $MAINLOG 2> >(sed -n '/Totals/,$p')
 
-### ========================= ###
-
-MAINLOG=$LOGDIR/otu-generation.log
-DEPRXDIR=/scratch/jenn/underc/deprx-then-trim/plantITS_FR_fq
+MAINLOG=$MAINDIR/logs/otu-generation.log
+mkdir -p logs; rm $MAINLOG
+DEPRXDIR=$MAINDIR/data/trim-deprx/plantITS
 
 SEP="\n\n===================================\n"
 echo -e "$SEP`date`  Merging with usearch fastq_mergepairs.\n" > $MAINLOG
@@ -52,63 +42,59 @@ echo -e "$SEP`date`  Converting to fasta with usearch fastq_filter.\n" >> $MAINL
 usearch -fastq_filter plantITS_merged.filt.fq -fastaout plantITS_merged.filt.fa >> $MAINLOG 2> >(sed -n '/Reads/,$p')
 echo -e "$SEP`date`  Converting to single-line fasta, manually, no thanks to usearch.\n" >> $MAINLOG
 $SCRIPTSDIR/unwrap-fasta.sh plantITS_merged.filt.fa > tmp; mv tmp plantITS_merged.filt.fa
-
-# Comparison of *uniquing* (technically dereplication, but not yet clustering)
-
 echo -e "$SEP`date`  Uniquing with YY's script; generates 'cast' file.\n" >> $MAINLOG
-perl $EDNA/unique.pl plantITS_merged.filt.fa plantITS_merged.filt.uniq.fa                      # YY
-# usearch -fastx_uniques plantITS_merged.filt.fa -sizeout -fastaout plantITS_merged.filt.uniq.fa    # JD
-
-# YY's script creates the cast file (list of reads aggregated) and sometimes picks different (later) representative sequences, but the number and size distros of reads are the same. // Actually, no, on the full set, his misses 17 sequences, including one with size=2280.
-
-# Now do different min-size 2 operations on each, since they had different steps.
+perl $EDNA/unique.pl plantITS_merged.filt.fa plantITS_merged.filt.uniq.fa
 echo -e "$SEP`date`  Removing singletons with usearch sortbysize.\n" >> $MAINLOG
-usearch -sortbysize plantITS_merged.filt.uniq.fa -fastaout plantITS_merged.filt.uniq.min2.fa -minsize 2 >> $MAINLOG 2> >(grep 'Sorting')         # YY
-# usearch -fastx_uniques plantITS_merged.filt.fa -minuniquesize 2 -sizeout -fastaout plantITS_merged.filt.uniq.min2.fa  # JD
-
-# These should now both be counted and sorted, and nearly identical. So, cluster them.
+usearch -sortbysize plantITS_merged.filt.uniq.fa -fastaout plantITS_merged.filt.uniq.min2.fa -minsize 2 >> $MAINLOG 2> >(grep 'Sorting')
 echo -e "$SEP`date`  Clustering with usearch cluster_otus.\n" >> $MAINLOG
-usearch -cluster_otus plantITS_merged.filt.uniq.min2.fa -otus otus.fa -uparseout uparse.txt -relabel YYOtu >> $MAINLOG 2> >(tr '\r' '\n' | grep '100.0%') # YY
-# Note that my version is going straight into a file called otus.id.fa, since I'm relabeling inside usearch instead of using rename_4.0.pl later.
-# usearch -cluster_otus plantITS_merged.filt.uniq.min2.fa -otus otus.fa -uparseout uparse.txt -relabel plantITS_OTU -sizeout -width 0 &   # JD
-
-# Regardless, we lose the size info. Grrr, usearch! -cluster_otus just doesn't include a -sizeout option.
-echo -e "$SEP`date`  Adding OTU cluster size back into headers with YY's rename_4.0.pl.\n" >> $MAINLOG
-perl $EDNA/rename_4.0.pl otus.fa OTU otus.id.fa   # YY
+usearch -cluster_otus plantITS_merged.filt.uniq.min2.fa -otus otus.fa -uparseout uparse.txt -relabel OTU >> $MAINLOG 2> >(tr '\r' '\n' | grep '100.0%')
+echo -e "$SEP`date`  Rewriting headers with YY's rename_4.0.pl.\n" >> $MAINLOG
+perl $EDNA/rename_4.0.pl otus.fa OTU otus.id.fa
 sed -i 's/^>_/>plantITS_/' otus.id.fa  # Underscore fix since we're prepending the primer name differently.
-
-echo -e "$SEP`date`  Doing something mysterious with usearch_global. Generates 'uc' file. \n" >> $MAINLOG
-$EDNA/usearch -usearch_global plantITS_merged.filt.fa -db otus.id.fa -strand plus -id 0.97 -uc otus.id.uc >> $MAINLOG 2> >(tr '\r' '\n' | grep '100.0%') # YY
-
-# Skipping the renaming Perl script on my version, because I did it directly in the -cluster_otus command.
-# Honestly, I'm not quite sure what this is doing, since it's not -cluster_otus. Where *is* the 0.97 in cluster_otus, above?
-# $EDNA/usearch -usearch_global plantITS_merged.filt.fa -db otus.id.fa -strand plus -id 0.97 -uc otus.id.uc # JD
-
+echo -e "$SEP`date`  Assigning original reads to clusters with usearch_global. Generates 'uc' file. \n" >> $MAINLOG
+$EDNA/usearch -usearch_global plantITS_merged.filt.fa -db otus.id.fa -strand plus -id 0.97 -uc otus.id.uc # JD
 echo -e "$SEP`date`  Summarizing 'uc' file with YY's one-liner. \n" >> $MAINLOG
-UC=otus.id.uc
-less -S $UC | perl -e 'while(<>){@s=split; if($s[-1] eq "*"){}else{$hash{$s[-1]} ++;} } foreach my $k (sort {$hash{$b}<=>$hash{$a}} keys %hash){ print "$k\t$hash{$k}\n"; }' > $UC.summary
-
-echo -e "$SEP`date`  Adding abundance (sequence count) info to OTU headers. Again." >> $MAINLOG
-perl $EDNA/add_abundance.pl otus.id.fa otus.id.uc.summary NA otus.id.abund.fa  # YY
-# perl $EDNA/add_abundance.pl otus.id.fa otus.id.uc.summary NA otus.id.abund.fa           # JD
-
-# And why aren't we just letting them do the OTU table? Oh, because it takes forever.
-# usearch -otutab plantITS_merged.fq -otus otus.fa -otutabout otutab.txt -notmatched unmapped.fa -dbmatched otus_with_sizes.fa -sizeout
-# or: J=cluster; sbatch -J $J -o $J.log -e $J.log --mem-per-cpu=16000m -n 1 -c 1 -t 24:00:00 <(echo '#!/bin/sh'; echo "usearch -otutab plantITS_merged.fq -otus otus.fa -otutabout otutab.txt -notmatched unmapped.fa -dbmatched otus_with_sizes.fa -sizeout -threads 1")
-# And that ended up doing it by RUNS, not SAMPLES! Bleh. Forget it.
+UC=otus.id.uc; less -S $UC | perl -e 'while(<>){@s=split; if($s[-1] eq "*"){}else{$hash{$s[-1]} ++;} } foreach my $k (sort {$hash{$b}<=>$hash{$a}} keys %hash){ print "$k\t$hash{$k}\n"; }' > $UC.summary
+echo -e "$SEP`date`  Adding abundance (sequence count) info to OTU headers." >> $MAINLOG
+perl $EDNA/add_abundance.pl otus.id.fa otus.id.uc.summary NA otus.id.abund.fa
+echo -e "$SEP`date`  Final size sort and unwrapping, generating final OTU FASTA ($SP.otus.fa)." >> $MAINLOG
+usearch -sortbysize otus.id.abund.fa -fastaout $SP.otus.fa -minsize 2
+$SCRIPTSDIR/unwrap-fasta.sh $SP.otus.fa > foo; mv foo $SP.otus.fa >> $MAINLOG 2>&1
 
 
-#############################################
-###  Comparative counts up to this point  ###
-#############################################
 
-# Counting stats for all steps
-grep -c '^@M' $DEPRXDATA/3*plantITS_R1.fq | sed 's|_plantITS_R1.fq:|\t|'  | tr '-' '\t' | sort -k1,1n -k2,2n | sed 's/\t/-/' | tr ' ' '\t' > $LOGDIR/01-deprimerplexed.counts
-grep '^@3' plantITS_merged.fq | cut -d '.' -f 1 | cut -c 2- | $SCRIPTSDIR/prettify.sh > $LOGDIR/02-merged.counts
-grep '^@3' plantITS_merged.filt.fq | cut -d '.' -f 1 | cut -c 2- | $SCRIPTSDIR/prettify.sh > $LOGDIR/03-filt.counts
-grep '^>3' plantITS_merged.filt.uniq.fa | cut -d '.' -f 1 | cut -c 2- | $SCRIPTSDIR/prettify.sh > $LOGDIR/04-uniq.counts
-grep '^>3' plantITS_merged.filt.uniq.min2.fa | cut -d '.' -f 1 | cut -c 2- | $SCRIPTSDIR/prettify.sh > $LOGDIR/05-min2.counts
-cp otus.id.uc.summary $LOGDIR/06-otus.counts
+#############################################################
+###  VSEARCH VERSION (mutually exclusive with the above)  ###
+#############################################################
 
-gzip plantITS_merged*f{a,q}  # Should be done with all the uncompressed fastx's now, so zip them up.
+echo -e "$LOGSEP`date`  Merging with vsearch fastq_mergepairs.\n" > $MAINLOG
+for F in $DEPRXDIR/3*_R1.fq; do 
+    R=`echo $F | sed 's|R1|R2|'`; LABEL=`basename $F | cut -d '_' -f 1 | tr '-' '_'`;
+    (echo; echo -e "Merging pair $F.\n") >> $MAINLOG;
+    vsearch --fastq_mergepairs $F --reverse $R --fastqout merged/$LABEL.mrgflt.fq \
+            --fastq_maxee 0.5 --fastq_maxns 0 --fastq_allowmergestagger --relabel "$LABEL.";
+done >> $MAINLOG 2>&1 &
 
+echo -e "$LOGSEP`date`  Concatenating sample-level merge files into plantITS.mergfilt.fq.\n" >> $MAINLOG 
+cat merged/*.fq > plantITS.mergfilt.fq
+
+echo -e "$LOGSEP`date`  Converting to fasta with vsearch fastq_filter; discarding sequences < 150 bp.\n" >> $MAINLOG
+vsearch --fastq_filter plantITS.mergfilt.fq --fastaout plantITS.mergfilt.fa --fasta_width 0 --fastq_minlen 150 >> $MAINLOG 2>&1
+
+echo -e "$LOGSEP`date`  Uniquing with vsearch derep_fulllength." >> $MAINLOG
+vsearch --derep_fulllength plantITS.mergfilt.fa --output plantITS.mergfilt.uniq.fa --fasta_width 0 --sizeout >> $MAINLOG 2>&1
+
+echo -e "$LOGSEP`date`  Compressing source FQ and FA." >> $MAINLOG
+gzip plantITS.mergfilt.fq plantITS.mergfilt.fa &
+
+N=8
+echo -e "$LOGSEP`date`  Denoising with vsearch cluster_unoise and keeping clusters of at least $N (default 8)." >> $MAINLOG
+vsearch --cluster_unoise plantITS.mergfilt.uniq.fa --centroids asvs.withchimeras.fa --fasta_width 0 --minsize $N --relabel preASV_ --sizein --sizeout >> $MAINLOG 2>&1
+
+# Chimera removal with uchime2 (ratio of 2, not 16); does sorting by sizein first
+echo -e "$LOGSEP`date`  Chimera removal with uchime2_denovo." >> $MAINLOG
+vsearch --uchime2_denovo asvs.withchimeras.fa --nonchimeras $SP.asvs.fa --fasta_width 0 --sizein --sizeout --relabel plantITS_ASV_ >> $MAINLOG 2>&1
+
+echo -e "$LOGSEP`date`  Searching for original reads with usearch_global using ASVs as database.\n" >> $MAINLOG
+vsearch --usearch_global plantITS.mergfilt.fa.gz --db $ALLASVFASTA --id 0.97 --otutabout $OTUTABLE >> $MAINLOG 2>&1 &
+# Optional at this point: sed -i '1s/#OTU ID/Primer_OTU/' $OTUTABLE
