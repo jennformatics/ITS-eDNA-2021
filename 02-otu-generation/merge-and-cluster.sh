d7925262a336002716efe68d6ee5b97ec4e4d6e2
Jenn Drummond <jenn.drummond@gmail.com>
2020-12-06 14:25:56 -0600

    Publication tidying, plus initial genericization from plantITS to arbitrary primer names

--- 02-otu-generation/merge-and-cluster.sh
+++ 02-otu-generation/merge-and-cluster.sh
@@ -2,25 +2,6 @@ SP=`basename $PWD`
 
 . ~/init/underc $SP
 
-# PROJNAME=underc
-# 
-# EDNA=$SOFTWARE/notre-dame/eDNA_pipeline_v3.7
-# D=/storage/hpc/work/spe1/data/edna
-# ACDIR=/projects/ac53/underc
-# PROJDIR=/projects/spe1/edna
-# METADIR=/projects/spe1/edna/metadata
-# 
-# SCRATCHDIR=/scratch/jenn/underc
-# MAINDIR=$SCRATCHDIR/$1
-# RSRCDIR=$MAINDIR/rsrc
-# SCRIPTSDIR=$MAINDIR/scripts
-# LOGDIR=$MAINDIR/logs
-# BLASTDIR=$MAINDIR/blast
-# DEPRXDATA=$MAINDIR/plantITS_FR_fq
-# 
-# cd $MAINDIR
-# echo "MAINDIR is now $MAINDIR."
-
 
 #####################################
 ###  Read merging and clustering  ###
@@ -29,30 +10,27 @@ SP=`basename $PWD`
 # Merging, filtering, and fq/fa conversion (keeping the filt.fq for later reference).
 # This is the crazy, hairy way to process stderr on its way into the log:   >> $MAINLOG 2> >(sed -n '/Totals/,$p')
 
-MAINLOG=$MAINDIR/logs/otu-generation.log
 mkdir -p logs; rm $MAINLOG
-DEPRXDIR=$MAINDIR/data/trim-deprx/plantITS
-
 SEP="\n\n===================================\n"
 echo -e "$SEP`date`  Merging with usearch fastq_mergepairs.\n" > $MAINLOG
-usearch -fastq_mergepairs $DEPRXDIR/3*_R1.fq -relabel @ -fastqout plantITS_merged.fq >> $MAINLOG 2> >(sed -n '/Totals/,$p')
+usearch -fastq_mergepairs $DEPRXDIR/3*_R1.fq -relabel @ -fastqout ${PRIMER}_merged.fq >> $MAINLOG 2> >(sed -n '/Totals/,$p')
 echo -e "$SEP`date`  Filtering with usearch fastq_filter.\n" >> $MAINLOG
-usearch -fastq_filter plantITS_merged.fq -fastq_maxee 0.5 -fastq_maxns 1 -fastqout plantITS_merged.filt.fq >> $MAINLOG 2> >(sed -n '/Reads/,$p')
+usearch -fastq_filter ${PRIMER}_merged.fq -fastq_maxee 0.5 -fastq_maxns 1 -fastqout ${PRIMER}_merged.filt.fq >> $MAINLOG 2> >(sed -n '/Reads/,$p')
 echo -e "$SEP`date`  Converting to fasta with usearch fastq_filter.\n" >> $MAINLOG
-usearch -fastq_filter plantITS_merged.filt.fq -fastaout plantITS_merged.filt.fa >> $MAINLOG 2> >(sed -n '/Reads/,$p')
+usearch -fastq_filter ${PRIMER}_merged.filt.fq -fastaout ${PRIMER}_merged.filt.fa >> $MAINLOG 2> >(sed -n '/Reads/,$p')
 echo -e "$SEP`date`  Converting to single-line fasta, manually, no thanks to usearch.\n" >> $MAINLOG
-$SCRIPTSDIR/unwrap-fasta.sh plantITS_merged.filt.fa > tmp; mv tmp plantITS_merged.filt.fa
+$SCRIPTSDIR/unwrap-fasta.sh ${PRIMER}_merged.filt.fa > tmp && mv tmp ${PRIMER}_merged.filt.fa
 echo -e "$SEP`date`  Uniquing with YY's script; generates 'cast' file.\n" >> $MAINLOG
-perl $EDNA/unique.pl plantITS_merged.filt.fa plantITS_merged.filt.uniq.fa
+perl $EDNA/unique.pl ${PRIMER}_merged.filt.fa ${PRIMER}_merged.filt.uniq.fa
 echo -e "$SEP`date`  Removing singletons with usearch sortbysize.\n" >> $MAINLOG
-usearch -sortbysize plantITS_merged.filt.uniq.fa -fastaout plantITS_merged.filt.uniq.min2.fa -minsize 2 >> $MAINLOG 2> >(grep 'Sorting')
+usearch -sortbysize ${PRIMER}_merged.filt.uniq.fa -fastaout ${PRIMER}_merged.filt.uniq.min2.fa -minsize 2 >> $MAINLOG 2> >(grep 'Sorting')
 echo -e "$SEP`date`  Clustering with usearch cluster_otus.\n" >> $MAINLOG
-usearch -cluster_otus plantITS_merged.filt.uniq.min2.fa -otus otus.fa -uparseout uparse.txt -relabel OTU >> $MAINLOG 2> >(tr '\r' '\n' | grep '100.0%')
+usearch -cluster_otus ${PRIMER}_merged.filt.uniq.min2.fa -otus otus.fa -uparseout uparse.txt -relabel OTU >> $MAINLOG 2> >(tr '\r' '\n' | grep '100.0%')
 echo -e "$SEP`date`  Rewriting headers with YY's rename_4.0.pl.\n" >> $MAINLOG
-perl $EDNA/rename_4.0.pl otus.fa OTU otus.id.fa
-sed -i 's/^>_/>plantITS_/' otus.id.fa  # Underscore fix since we're prepending the primer name differently.
+perl $EDNA/rename_4.0.pl otus.fa ${PRIMER}_OTU otus.id.fa
+sed -i "s/^>_/>/" otus.id.fa  # Underscore fix
 echo -e "$SEP`date`  Assigning original reads to clusters with usearch_global. Generates 'uc' file. \n" >> $MAINLOG
-$EDNA/usearch -usearch_global plantITS_merged.filt.fa -db otus.id.fa -strand plus -id 0.97 -uc otus.id.uc # JD
+$EDNA/usearch -usearch_global ${PRIMER}_merged.filt.fa -db otus.id.fa -strand plus -id 0.97 -uc otus.id.uc # JD
 echo -e "$SEP`date`  Summarizing 'uc' file with YY's one-liner. \n" >> $MAINLOG
 UC=otus.id.uc; less -S $UC | perl -e 'while(<>){@s=split; if($s[-1] eq "*"){}else{$hash{$s[-1]} ++;} } foreach my $k (sort {$hash{$b}<=>$hash{$a}} keys %hash){ print "$k\t$hash{$k}\n"; }' > $UC.summary
 echo -e "$SEP`date`  Adding abundance (sequence count) info to OTU headers." >> $MAINLOG
@@ -75,26 +53,26 @@ for F in $DEPRXDIR/3*_R1.fq; do
             --fastq_maxee 0.5 --fastq_maxns 0 --fastq_allowmergestagger --relabel "$LABEL.";
 done >> $MAINLOG 2>&1 &
 
-echo -e "$LOGSEP`date`  Concatenating sample-level merge files into plantITS.mergfilt.fq.\n" >> $MAINLOG 
-cat merged/*.fq > plantITS.mergfilt.fq
+echo -e "$LOGSEP`date`  Concatenating sample-level merge files into ${PRIMER}.mergfilt.fq.\n" >> $MAINLOG 
+cat merged/*.fq > ${PRIMER}.mergfilt.fq
 
 echo -e "$LOGSEP`date`  Converting to fasta with vsearch fastq_filter; discarding sequences < 150 bp.\n" >> $MAINLOG
-vsearch --fastq_filter plantITS.mergfilt.fq --fastaout plantITS.mergfilt.fa --fasta_width 0 --fastq_minlen 150 >> $MAINLOG 2>&1
+vsearch --fastq_filter ${PRIMER}.mergfilt.fq --fastaout ${PRIMER}.mergfilt.fa --fasta_width 0 --fastq_minlen 150 >> $MAINLOG 2>&1
 
 echo -e "$LOGSEP`date`  Uniquing with vsearch derep_fulllength." >> $MAINLOG
-vsearch --derep_fulllength plantITS.mergfilt.fa --output plantITS.mergfilt.uniq.fa --fasta_width 0 --sizeout >> $MAINLOG 2>&1
+vsearch --derep_fulllength ${PRIMER}.mergfilt.fa --output ${PRIMER}.mergfilt.uniq.fa --fasta_width 0 --sizeout >> $MAINLOG 2>&1
 
 echo -e "$LOGSEP`date`  Compressing source FQ and FA." >> $MAINLOG
-gzip plantITS.mergfilt.fq plantITS.mergfilt.fa &
+gzip ${PRIMER}.mergfilt.fq ${PRIMER}.mergfilt.fa &
 
 N=8
 echo -e "$LOGSEP`date`  Denoising with vsearch cluster_unoise and keeping clusters of at least $N (default 8)." >> $MAINLOG
-vsearch --cluster_unoise plantITS.mergfilt.uniq.fa --centroids asvs.withchimeras.fa --fasta_width 0 --minsize $N --relabel preASV_ --sizein --sizeout >> $MAINLOG 2>&1
+vsearch --cluster_unoise ${PRIMER}.mergfilt.uniq.fa --centroids asvs.withchimeras.fa --fasta_width 0 --minsize $N --relabel preASV_ --sizein --sizeout >> $MAINLOG 2>&1
 
 # Chimera removal with uchime2 (ratio of 2, not 16); does sorting by sizein first
 echo -e "$LOGSEP`date`  Chimera removal with uchime2_denovo." >> $MAINLOG
-vsearch --uchime2_denovo asvs.withchimeras.fa --nonchimeras $SP.asvs.fa --fasta_width 0 --sizein --sizeout --relabel plantITS_ASV_ >> $MAINLOG 2>&1
+vsearch --uchime2_denovo asvs.withchimeras.fa --nonchimeras $SP.asvs.fa --fasta_width 0 --sizein --sizeout --relabel ${PRIMER}_ASV_ >> $MAINLOG 2>&1
 
 echo -e "$LOGSEP`date`  Searching for original reads with usearch_global using ASVs as database.\n" >> $MAINLOG
-vsearch --usearch_global plantITS.mergfilt.fa.gz --db $ALLASVFASTA --id 0.97 --otutabout $OTUTABLE >> $MAINLOG 2>&1 &
+vsearch --usearch_global ${PRIMER}.mergfilt.fa.gz --db $ALLASVFASTA --id 0.97 --otutabout $OTUTABLE >> $MAINLOG 2>&1 &
 # Optional at this point: sed -i '1s/#OTU ID/Primer_OTU/' $OTUTABLE
--- 03-otu-id/blast-and-after.sh
+++ 03-otu-id/blast-and-after.sh
@@ -1,32 +1,9 @@
-. ~/init/underc
+#!/bin/bash
 
-EDNA=$HOME/spe-software/notre-dame/eDNA_pipeline_v3.7
-D=/storage/hpc/work/spe1/data/edna
-ACDIR=/projects/ac53/underc
-PROJDIR=/projects/spe1/edna
-METADIR=/projects/spe1/edna/metadata
+SP=`basename $PWD`
 
-SCRATCHDIR=/scratch/jenn/underc
-MAINDIR=$SCRATCHDIR/sixlakes  # wkg-td
-RSRCDIR=$MAINDIR/rsrc
-SCRIPTSDIR=$MAINDIR/scripts
-LOGDIR=$MAINDIR/logs
-BLASTDIR=$MAINDIR/blast
-DEPRXDATA=$MAINDIR/plantITS_FR_fq
+. ~/init/underc $SP
 
-mkdir -p $MAINDIR/blast
-mkdir -p $LOGDIR/deprx
-cd $MAINDIR
-ln -s /home/jenn/scripts/underc scripts
-mkdir -p rsrc; cd rsrc
-for R in $PROJDIR/rsrc/*.{dmp,fas}; do ln -s $R; done
-ln -s $METADIR/sample-names-and-order.txt
-# Keep this as a backup somewhere: tar -hczf underc-basic-rsrc.tgz rsrc
-
-# OR:
-cd $MAINDIR
-rmdir rsrc
-tar -xzf $PROJDIR/metadata/underc-basic-rsrc.tgz
 
 #####################################
 ###  BLAST lookup and annotation  ###
@@ -34,43 +11,66 @@ tar -xzf $PROJDIR/metadata/underc-basic-rsrc.tgz
 
 # blastn -task blastn -max_target_seqs 20 -remote -db nt -outfmt '6 qseqid sseqid sacc staxids sscinames scomnames sblastnames pident nident evalue score bitscore'
 
-cd $MAINDIR
-ln -s otus.id.fa asvs.fasta
-# or temporarily,  head -n 200 otus.id.abund.fa > asvs.fasta
+ALLOTUFASTA=$MAINDIR/$SP.otus.fa
+BLASTFILE=$BLASTDIR/$SP.new.otus.blast
 
-cat $MAINDIR/asvs.fasta | tr '\n' ' ' | sed 's/>/\n>/g' | awk '{FS=" "} {printf("%s\t%s\n", $1, length($2))}' | sed 's/;size=[0-9]*//g' | sed '1d' | cut -c 2- | sed 's/;//' > $RSRCDIR/asv-sequence-lengths.txt
-cd $BLASTDIR
+mkdir -p $BLASTDIR; cd $BLASTDIR
 
 # Do this only once, or you'll overwrite your results.
-echo "ASV|GenBankGI|GenBankNucl|Taxon Acc|Sci Name|Common Name|Taxon Category|Pct Ident|Len Ident|e-Value|Score|Bitscore" | tr '|' '\t' > all-asvs.blast
+echo "Primer_OTU_Size|GenBankGI|GenBankNucl|Taxon Acc|Sci Name|Common Name|Taxon Category|Pct Ident|Len Ident|e-Value|Score|Bitscore" | tr '|' '\t' > $BLASTFILE
+
+# First, see if some of the OTUs have previously been BLASTed and evaluated.
+
+OTUDBDIR=/scratch/jenn/underc/universal-otus
+DBFILE=$OTUDBDIR/universal-otu-db.txt
+OCCFILE=$OTUDBDIR/occurrences-db.txt
+
+ALLOTUFASTA=$MAINDIR/$SP.otus.fa
+NEWOTUFASTA=$BLASTDIR/$SP.new.otus.fa
+BLASTFILE=$BLASTDIR/$SP.new.otus.blast
+BLASTEVALFILE=$BLASTDIR/$SP.new.otus.blast.evaluated
+SEQFILENAME=$SP.otus.seq
+
+sed -n '2~2p' $ALLOTUFASTA > $SEQFILENAME
+grep -F -wf <(cut -f 29 $DBFILE) $SEQFILENAME > already-in-db.seq
+grep -F -wvf <(cut -f 29 $DBFILE) $SEQFILENAME > not-in-db.seq
+
+# Put a second record in OCCFILE for the ones already in DBFILE.
+grep -Fwf already-in-db.seq -B 1 --no-group-separator $ALLOTUFASTA \
+    | tr '\n' '|' | sed 's/|>/\n/g' | sed 's/>//' | tr '|' '\t' | sed 's/\t$//' \
+    | sed "s|^|$ALLOTUFASTA\t|" >> $OCCFILE   # Be careful not to do this twice; if you do, clean up with sort -u.
+echo >> $OCCFILE  # For some reason there's no trailing newline.
+
+# Create the FASTA of new sequences for BLASTing
+grep -B 1 --no-group-separator -Fwf not-in-db.seq $ALLOTUFASTA > $NEWOTUFASTA
 
 mkdir -p $BLASTDIR/split; cd $BLASTDIR/split
-split -l 200 --numeric-suffixes=1 $MAINDIR/asvs.fasta OTUs.subset.
+split -l 100 --numeric-suffixes=1 $NEWOTUFASTA OTUs.subset.
 for F in OTUs.subset.*; do NEWNAME=`echo $F | sed 's/\.0*/./g'`; if [ "$F" != "$NEWNAME" ]; then mv $F $NEWNAME; fi; done
 
-# for F in /projects/ac53/underc/analysis/rsrc/databases/ncbi-taxdb/taxdb*; do ln -s `readlink -m $F`; done
+# BLAST the new OTUs. Will generate OTUs.subset.XX.blast.
 cp /storage/hpc/work/spe1/blast/taxdb.tar.gz .; tar xzf taxdb.tar.gz; rm taxdb.tar.gz
 ARRAYNUMS=`ls OTUs.subset.* | cut -d '.' -f 3 | sort -n | tr '\n' ',' | sed 's/,$//'`
 sbatch -a "$ARRAYNUMS%10" -n 1 -c 1 --mem-per-cpu=2000m -t 04:00:00 $SCRIPTSDIR/remote-blast.sh
 
-###  To repeat until done (because of inevitable errors)  ###
+###  If partial repeat is needed due to errors, repeat starts here  ###
 
 # After BLAST is done, append the results, in order, to the master file. Verify the results, then remove the split directory.
-cd $BLASTDIR/split
-for N in `ls OTUs.subset.* | cut -d '.' -f 3 | sort -n`; do cat OTUs.subset.$N.blast; done >> ../all-asvs.blast
+cd $BLASTDIR/split   # In case you went somewhere else while waiting for the BLAST.
+for N in `ls OTUs.subset.* | cut -d '.' -f 3 | sort -n`; do cat OTUs.subset.$N.blast; done >> $BLASTFILE
 
 # See how many BLASTs succeeded, and which failed.
-diff <(cat $BLASTDIR/all-asvs.blast | cut -f 1 | sort -u) <(grep '^>' $MAINDIR/asvs.fasta | cut -c 2- | sort) | grep '^> ' | cut -c 3- > failures.txt
+diff <(cat $BLASTFILE | cut -f 1 | sort -u) <(grep '^>' $NEWOTUFASTA | cut -c 2- | sort) | grep '^> ' | cut -c 3- > failures.txt
 
-# Cleanup: make sure you've copied the results into all-asvs.blast before doing the following!
+# Cleanup: make sure you've copied the results into $BLASTFILE before doing the following!
+N=1  # Increment with every rerun.
 mkdir -p logs; mv *.out logs;
-N=2
 mkdir -p blastruns/$N; mv *.blast blastruns/$N; find blastruns/$N -size 0 -exec rm {} \;
-rm *.subset.* *.fasta
+rm *.subset.*
 
-# Make a new FASTA with the failures, split it, and run again.
-grep --no-group-separator -w -A 1 -f failures.txt $MAINDIR/asvs.fasta > OTUs.fasta
-split -l 200 --numeric-suffixes=1 OTUs.fasta OTUs.subset.
+# IF THERE WERE FAILURES: Make a new FASTA with the failures, split it, and run again.
+grep --no-group-separator -w -A 1 -f failures.txt $ALLOTUFASTA > tmp.reblast.fasta
+split -l 200 --numeric-suffixes=1 tmp.reblast.fasta OTUs.subset.
 for F in OTUs.subset.*; do NEWNAME=`echo $F | sed 's/\.0*/./g'`; if [ "$F" != "$NEWNAME" ]; then mv $F $NEWNAME; fi; done
 ARRAYNUMS=`ls *.subset.* | cut -d '.' -f 3 | sort -n | tr '\n' ',' | sed 's/,$//'`
 sbatch -a "$ARRAYNUMS%5" -n 1 -c 1 --mem-per-cpu=4000m -t 04:00:00 ~/scripts/underc/remote-blast.sh
@@ -80,11 +80,13 @@ sbatch -a "$ARRAYNUMS%5" -n 1 -c 1 --mem-per-cpu=4000m -t 04:00:00 ~/scripts/und
 ###  End repeat  ###
 
 cd $MAINDIR
-cat otus.id.abund.fa | tr '\n' ' ' | sed 's/>/\n>/g' | awk '{FS=" "} {printf("%s\t%s\n", $1, length($2))}' | sed 's/;size=[0-9]*;*//g' | sed '1d' | cut -c 2- > $RSRCDIR/otu-sequence-lengths.txt
-cd $BLASTDIR; rm -rf split; ln -s $RSRCDIR
-cut -f 4 all-asvs.blast | sed '1d' | tr ';' '\n' | sort -u | grep -v 'None' | $SCRIPTSDIR/get-taxonomy.py > $RSRCDIR/taxonomy-trees.txt
+wc -l $RSRCDIR/taxonomy-trees.txt
+cut -f 4 $BLASTFILE | sed '1d' | tr ';' '\n' | sort -u | grep -v 'None' | $SCRIPTSDIR/get-taxonomy.py > local-taxonomy-trees.txt
+cat local-taxonomy-trees.txt >> $RSRCDIR/taxonomy-trees.txt; sort -u $RSRCDIR/taxonomy-trees.txt > foo; mv foo $RSRCDIR/taxonomy-trees.txt
+wc -l $RSRCDIR/taxonomy-trees.txt
 
-$SCRIPTSDIR/evaluate-blast.py --rsrcdir ../rsrc --blastdir .   # Generates all-asvs.blast.evaluated
+cd $BLASTDIR
+$SCRIPTSDIR/evaluate-blast.py -b $BLASTFILE -o $ALLOTUFASTA -e $BLASTFILE.evaluated   # Generates $BLASTFILE.evaluated
 
 # evaluate-blast.py    (jd)
 #  * Collects summary data for the up to 20 returned BLAST hits for each OTU
@@ -96,6 +98,31 @@ $SCRIPTSDIR/evaluate-blast.py --rsrcdir ../rsrc --blastdir .   # Generates all-a
 #  * Calculates various summary stats across OTUs and organisms
 #  * Adds full tree of canonical taxon levels (K/P/C/O/F/G/S) from NCBI taxdmp
 
+# Now put those new evaluated BLAST results in the universal database.
+OTUDB=/scratch/jenn/underc/universal-otus
+DBFILE=$OTUDB/universal-otu-db.txt
+OCCFILE=$OTUDB/occurrences-db.txt
+ALLOTUFASTA=`readlink -m $SP.otus.fa`
+NEWOTUFASTA=`readlink -m $SP.new.otus.fa`
+BLASTFILE=`readlink -m $BLASTDIR/$SP.new.otus.blast`
+BLASTEVALFILE=`readlink -m $BLASTFILE.evaluated`
+
+# Put NEW OTUs ONLY in the database.
+# Put OTU and its ID on one line, then prepend OTU file source path, then the evaluated BLAST data which now includes the sequence.
+cat $NEWOTUFASTA | tr '\n' '|' | sed 's/|>/\n/g' | sed 's/|/\t/g' | sed 's/>//g' | sed "s|^|$ALLOTUFASTA\t|" \
+    | while read P OTU SEQ; do echo -e "`date '+%Y-%m-%d %H:%m'`\t$P\t`grep -w $OTU $BLASTEVALFILE`"; done | grep -vP '\t$' >> $DBFILE &
+
+# Put ALL OTUs in the occurrences list.
+# Same for OCCFILE, though we're using the whole OTU file rather than just the newly-BLASTED ones.
+(cat $ALLOTUFASTA | tr '\n' '|' | sed 's/|>/\n/g' | sed 's/>//' | tr '|' '\t' | sed 's/\t$//' | sed "s|^|$ALLOTUFASTA\t|"; echo) >> $OCCFILE
+
+# Now USE the database to generate $SP.prev.otus.blast.evaluated for the previous set.
+cd $BLASTDIR
+cat already-in-db.seq | while read SEQ; do LOCALOTU=`grep -Fw $SEQ $OCCFILE | grep "$SP" | cut -f 2 | head -n 1`; grep -Fw $SEQ $DBFILE | cut -f 4-29 | sed "s/^/$LOCALOTU\t/"; done > $SP.prev.otus.blast.evaluated &
+
+# $SP.new.otus.blast has a header, from the fresh BLAST we just did, and ...evaluated doesn't, which is perfect.
+cat $BLASTEVALFILE $SP.prev.otus.blast.evaluated > $BLASTDIR/$SP.blast.evaluated
+
 
 ######################################################
 ###  Manual creation of OTU table plus taxon info  ###
@@ -103,35 +130,33 @@ $SCRIPTSDIR/evaluate-blast.py --rsrcdir ../rsrc --blastdir .   # Generates all-a
 
 cd $MAINDIR
 
-cat otus.id.abund.fa | tr '\n' ' ' | sed 's/>/\n>/g' | awk '{FS=" "} {printf("%s\t%s\n", $1, length($2))}' | sed 's/;size=[0-9]*;*//g' | sed '1d' | cut -c 2- > $RSRCDIR/otu-sequence-lengths.txt
+# Generate raw data for OTU table by summarizing the very verbose ".uc" file.
+cut -f 9,10 otus.id.uc | tr '.' '\t' | cut -f 1,3 | ~/scripts/underc/pretty-count.sh | sed 's/  */\t/g' | awk '{print $2, $3, $1}' > fine-scaled-read-quantities.txt
 
-# Generate raw data for OTU table by comparing the very verbose ".uc" file with the ordered list of samples.
-rm progress.log fine-scaled-read-quantities.txt
-cut -f 1 $RSRCDIR/sample-names-and-order.txt | sed '1d' | sort -t'-' -k1,1n -k2,2n | \
-    while read SAMPLENAME; do echo $SAMPLENAME >> progress.log; \
-        cut -f 9,10 otus.id.uc | grep -w "$SAMPLENAME" | tr '.' '\t' | cut -f 1,3 | \
-        ~/scripts/pretty-count.sh | sed 's/  */\t/g' | awk '{print $2, $3, $1}' | sort -t'_' -k2,2n | \
-        grep -v '\*' >> fine-scaled-read-quantities.txt; done &
-tail -f progress.log
+OTUTABLE=$SP.otu-table.txt
 
 # Rearrange that "long" file into "wide" format.
-~/scripts/melt.py 2 1 fine-scaled-read-quantities.txt > tmp; head -n 1 tmp | sed 's/Index/Primer_OTU/' > otu-table.txt; sed '1d' tmp | sort -t'_' -k3,3n >> otu-table.txt; rm tmp
+$HOME/scripts/melt.py 2 1 fine-scaled-read-quantities.txt > tmp; head -n 1 tmp | sed 's/Index/Primer_OTU/' > $OTUTABLE; sed '1d' tmp | sed "s/^_/${PRIMER}_/" | sort -t'_' -k3,3n >> $OTUTABLE; rm tmp
 
-# Create source files that will allow integration script to spread known taxon categories to related taxa that appear elsewhere in the file (maybe should write this functionality in to evaluate-blast.py).
-BLASTEVAL=blast/AllOTUs.blast.evaluated; cat $BLASTEVAL | grep 'needs lookup' | cut -f 5 | cut -d ' ' -f 1 | sort -u | while read TAXON; do grep -w "$TAXON" $BLASTEVAL | cut -f 7 | sort -u | grep -v 'needs lookup' | sed "s/^/$TAXON\t/"; done > taxcats.txt
+# Create source files that will allow integration script to spread known taxon categories to related taxa that appear elsewhere in the file (should write this functionality in to evaluate-blast.py, along with handling of taxa that aren't actually very similar OTUs even though they share a high-level taxid).
+BLASTEVAL=$BLASTDIR/$SP.blast.evaluated
+cat $BLASTEVAL | grep 'needs lookup' | cut -f 5 | cut -d ' ' -f 1 | sort -u | while read TAXON; do grep -w "$TAXON" $BLASTEVAL | cut -f 7 | sort -u | grep -v 'needs lookup' | sed "s/^/$TAXON\t/"; done > taxcats.txt
 cut -f 1 taxcats.txt | sort | uniq -c | grep -v ' 1 ' | cut -c 9- > multcats.txt
 grep -wvf multcats.txt taxcats.txt > foo; mv foo taxcats.txt
 cat multcats.txt | sed 's/$/\tmultiple/' >> taxcats.txt
 
-$SCRIPTSDIR/integrate-otus-and-samples.py
+$SCRIPTSDIR/integrate-otus-and-samples.py -s $OTUTABLE -b $BLASTEVAL -p $SP -o $ALLOTUFASTA
 
 cd $MAINDIR
 gzip *.fq *.uc *.cast uparse.txt &
-rm progress.log multcats.txt taxcats.txt
+rm fine-scaled-read-quantities.txt local-* progress.log multcats.txt taxcats.txt
+mkdir intermediates
+mv otus.fa otus.id.* ${PRIMER}_* uparse* intermediates
+tar -czf intermediates.tgz intermediates && rm -rf intermediates
 
-cp blast/ambiguity-report.txt blast/AllOTUs.blast.evaluated .
-tar czf output-bundle.tgz cumulative* otu-table.txt otus.id.abund.fa ambiguity-report.txt ambiguity-report.txt AllOTUs.blast.evaluated logs
-rm $MAINDIR/ambiguity-report.txt $MAINDIR/AllOTUs.blast.evaluated
+cp blast/ambiguity-report.txt $BLASTEVAL .
+tar czf output-bundle.tgz $SP.* $OTUFASTA ambiguity-report.txt $SP.blast.evaluated logs
+rm $MAINDIR/ambiguity-report.txt $MAINDIR/$SP.blast.evaluated 
 
 
 #integrate-orgs-and-samples.py   (jd -- combines the BLAST results with the OTU-by-sample info and NCBI taxdmp)
@@ -155,4 +180,15 @@ rm $MAINDIR/ambiguity-report.txt $MAINDIR/AllOTUs.blast.evaluated
 ###  Auto-curation, to the extent possible  ###
 ###############################################
 
-cat cumulative.4.organisms.passing
+cd $MAINDIR
+
+for SP in amph; do 
+(cd $SP; echo $SP;
+    PASSING=$SP.4.organisms.passing
+    CURATED=$SP.5.orgs.curated
+    grep -f $RSRCDIR/autocurate-yes.txt $PASSING > tmp1
+    grep -vf $RSRCDIR/autocurate-no.txt $PASSING | grep -wvf $RSRCDIR/taxon-exclusions.txt > tmp2
+    cat tmp1 tmp2 | sort -u > $CURATED
+    # sed -i "s/Richardson's pondweed/\"Richardson's pondweed\"/" $CURATED); done
+
+rm tmp1 tmp2
